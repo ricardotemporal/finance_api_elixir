2508e5c1f54fb420225836c32bb2ddac57487f8b
Ricardo <ricardotemporal10@gmail.com>
2025-06-19 19:41:03 -0300

    generate User controller, views and RESTful routes

--- lib/finance_api_web/router.ex
+++ lib/finance_api_web/router.ex
@@ -7,6 +7,8 @@ defmodule FinanceApiWeb.Router do
 
   scope "/api", FinanceApiWeb do
     pipe_through :api
+
+    resources "/users", UserController, except: [:new, :edit]
   end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
